324f81c494154053175b97c7b4126a67c707ce78
JV <jaiveer.chadda@gmail.com>
2026-05-23 17:30:26 +0100

    I genuinely can't remember what I was doing, but I'm just gonna commit now. I'll probably come back to it soon / at some point

---
+++ source/columns.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+# -c  -- format output to fit display of specified width
+# -s  -- specify column delimiters in input data
+# -t  -- create a table
+# -x  -- print across before down
--- source/simple.zsh
+++ source/simple.zsh
@@ -4,31 +4,130 @@
 # Possible Inputs & Outputs
 # ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 #   columns               ->   column -t -s $_dflt_sep
-#   columns -s '...'      ->   column -t -s '...'
 #   columns '...'         ->   column -t -s '...'
-# × columns -t            ->   column -t
-# × columns -t -s '...'   ->   column -t -s '...'
-#
-# × columns -c 100        ->   column -c 100
-# × columns -x            ->   column -x 
+#   columns -s '...'      ->   column -t -s '...'
+
+# × columns -t            ->   back to `column`
+# × columns -t -s         ->   back to `column`
+# × columns -t -s '...'   ->   back to `column`
+# × columns -ts '...'     ->   back to `column`
+# × columns -st '...'     ->   back to `column`
+# × columns -x            ->   back to `column`
+# × columns -c 100        ->   back to `column`
+# × columns -cdstx        ->   back to `column`
+# × columns -cdstx '...'  ->   back to `column`
 
 
-columns::simple() {
+
+columns::test() {
+
+  local -rA _func_args=(
+    ["a              "]='us'
+    ["b '...'        "]='us'
+    ["c -s '...'     "]='us'
+    ["d -            "]='us'
+    ["e '-'          "]='us'
+    ["f file2.txt    "]='us'
+    ["g -s           "]='them'
+    ["h file.txt     "]='them'
+    ["i -file.txt    "]='them'
+    ["j -sfile.txt   "]='them'
+    ["k -file2.txt   "]='them'
+    ["l -sfile2.txt  "]='them'
+    ["m -t           "]='them'
+    ["n -t -s        "]='them'
+    ["o -t -s '...'  "]='them'
+    ["p -ts '...'    "]='them'
+    ["q -st '...'    "]='them'
+    ["r -x           "]='them'
+    ["s -c 100       "]='them'
+    ["t -cdstx '...' "]='them'
+    ["u -scdtx       "]='them'
+  )
+
+  local function args exp_result result
+  for args in "${(@ko)_func_args}"; do
+
+    exp_result="${_func_args[$args]}"
+    function="columns ${args[3,-1]}"
+
+    echo "$function" \
+      | bat -p --color='always' -l='zsh' \
+      | perl -pe 'chomp if eof'
+
+    result=$( "${(z)function}" )
+    echo -n "${(r:100:: :)exp_result}"
+
+    [[ "$result" == "$exp_result" ]] \
+      && echo -ne $'\e[32m --> \e[0m' \
+      || echo -ne $'\e[31m --> \e[0m'
+
+    echo "$result"
+  done
+}
+
+
+
+columns() {
+
+
+  echo -n "${(j: \e[31m•\e[39m :)@}\t\t\t\t" >&2 
+
+  local -r _dflt_sep=' '
+  # local -r _unimpl_args='^-(c|x)$'
+  local -ri 10 _delim=$RANDOM
+
+  # if $1 is a file, send it to the main function
+
+  [[ -e "$1"
+  || -z "$1"
+  || ( "$1" == '-s' && "$2" )
+  || "$1" =~ '^-'
+
+  ]]\
+  && echo them || echo us
+
+  #{
+  #   echo 'send it over to the main args'
+  #   return
+  # }
+
+  # # if $1
+  # #  • is '-s', or
+  # #  • starts with a hyphen
+
+  # # # [[ "$1" =~ '^((-s)?$|[^-])' ]] && {
+  # # [[ "$1" =~ '^(-s$|[^-])' ]] && {
+  # #   echo "we're running it"
+  # #   return
+  # # }
+
+  # echo 'unrecognised flag'
+
+  return
+
+
+
+
+
+  # echo "${(pj:$_delim:)@}"
+  # (( $@[(I)$~_unimpl_args] )) && echo yes || echo no
+
+
+
 
   (( $# >= 2 )) && { column "$@"; return; }
-  # $sep will remain as a space if $1 isn't given
-  # or if $# == 0
+
+
+
+  # [[  ]]
+
   local sep="${1:- }"
-  # "(( $# ))" means: "if $# != 0",
-  #  but since we've checked whether $# >= 2 already,
-  #  this check is actually asking if $# == 1
-  (( $# )) && {
-      case "$sep" in
-          t) sep=$'\t' ;;
-          n) sep=$'\n' ;;
-      esac
-  }
+  [[ "$sep" == 't' ]] && sep=$'\t'
+
   # I'll figure out the exact syntax later
   #  cos I'll need to read stuff infrom stdin
   column -t -s "$sep"
 }
+
+# spell-checker:ignoreRegExp /(?<=\s#.*)\w|[-]\w+\b/g
